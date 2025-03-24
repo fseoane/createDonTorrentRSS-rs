838adba8a1e99b6c7486c44dace5423ac070b9a2
Fernando Seoane <fseoane@hotmail.com>
2025-03-24 09:16:49 +0100

    ...

--- build.Alpine.sh
+++ build.Alpine.sh
@@ -1,5 +1,5 @@
 sudo apk add pkgconfig
 sudo apk add gcc musl-dev openssl openssl-dev
-#  remember to add a dependency in cargo:
+#  remember to add a dependency in cargo.toml:
 #       git2 = {version="0.13.22", features = ["vendored-libgit2"]}
 RUSTFLAGS="-Ctarget-feature=-crt-static" cargo build --release
